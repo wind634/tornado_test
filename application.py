c2b37ffde808d1234032ab96dc76260b90cba477
wind634 <2911184332@qq.com>
2017-05-02 13:45:01 +0800

    项目目录调整

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from handlers.index import IndexHandler
 from url import url
 
 import tornado.web
@@ -6,12 +7,18 @@ import os
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = url
+        # handlers = url
+    
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True
         )
+        
+        handlers = [
+            (r'/', IndexHandler),
+        ]
 
-        tornado.web.Application.__init__(self, handlers, settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
         
         # self.db =
