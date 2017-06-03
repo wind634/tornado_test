9ac228efca09f7bf254c58fc400120b68e2b6fac
wind634 <2911184332@qq.com>
2017-06-03 21:18:02 +0800

    项目目录调整

--- tests/test03.py
+++ tests/test03.py
@@ -3,8 +3,8 @@
     测试python调用java
 """
 
-from jpype import *
-
-startJVM(getDefaultJVMPath(), "-ea")
-java.lang.System.out.println("hello world")
-shutdownJVM()
+# from jpype import *
+#
+# startJVM(getDefaultJVMPath(), "-ea")
+# java.lang.System.out.println("hello world")
+# shutdownJVM()
