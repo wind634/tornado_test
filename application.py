dfb14ae081530e9721fa1d0e5bc020b7419fabb6
wind634 <2911184332@qq.com>
2017-06-07 15:10:40 +0800

    项目目录调整

--- application.py
+++ application.py
@@ -1,29 +1,31 @@
-from url import url
-
 import tornado.web
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from tornado.options import options
+from tornado.options import define, options
 
-print(options)
 
-engine = None
+define("port", default=8888, help="run on th given port", type=int)
+define("db_host", default="127.0.0.1", type=str)
+define("db_user", default="root", type=str)
+define("db_pwd", default="root", type=str)
+define("db_name", default="testdb", type=str)
+define("db_prot", default=3306, type=int)
 
-# engine = create_engine(
-#             'mysql+pymysql://%s:%s@%s:%d/%s' % (
-#                 options.db_user,
-#                 options.db_pwd,
-#                 options.db_host,
-#                 options.db_port,
-#                 options.db_name,
-#             ),
-#             encoding='utf-8',
-#             echo=False,
-#             pool_size=100,
-#             pool_recycle=7200,
-#             connect_args={'charset':'utf8'},
-#         )
+
+engine = create_engine(
+            'mysql+pymysql://%s:%s@%s:%d/%s' % (
+                options.db_user,
+                options.db_pwd,
+                options.db_host,
+                options.db_port,
+                options.db_name,
+            ),
+            encoding='utf-8',
+            echo=False,
+            pool_size=100,
+            pool_recycle=7200,
+            connect_args={'charset':'utf8'},
+        )
 
 
 class Application(tornado.web.Application):
--- settings.py
+++ settings.py
@@ -1,15 +1,5 @@
 import os
-from tornado.options import define
-
 base_dir = os.path.dirname(__file__)
-
-define("port", default=8888, help="run on th given port", type=int)
-define("db_host", default="127.0.0.1", type=str)
-define("db_user", default="root", type=str)
-define("db_pwd", default="root", type=str)
-define("db_name", default="testdb", type=str)
-define("db_prot", default=3306, type=int)
-
 
 settings = dict(
     template_path=os.path.join(base_dir, "templates"),
